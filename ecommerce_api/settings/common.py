886b7db90f105ff341af131145cb652ae707ee92
eliaszayoud-SE <eng.eliaszayoud@gmail.com>
2024-07-18 11:46:54 +0300

    fix user create error

--- ecommerce_api/settings/common.py
+++ ecommerce_api/settings/common.py
@@ -150,7 +150,7 @@ SIMPLE_JWT = {
 DJOSER = {
     
     'SERIALIZERS': {
-        'user_create':'core.serializers.UserCreateSerializer',
+        'user_create':'api.serializers.UserCreateSerializer',
     },
 }
 
