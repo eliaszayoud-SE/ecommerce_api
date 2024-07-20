772ea275ca0d7a1f1605bd8079cd7a0c06528c4a
eliaszayoud-SE <eng.eliaszayoud@gmail.com>
2024-07-20 23:48:18 +0300

    create Item table

--- store/models.py
+++ store/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    name_ar = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='category/images')
+    date = models.DateField(auto_now_add=True)
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    name_ar = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    description_ar = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='items/image')
+    count = models.IntegerField()
+    active = models.BooleanField(default=True)
+    price = models.FloatField()
+    discount = models.SmallIntegerField()
+    date = models.DateField(auto_now_add=True)
