e3028e1b3752ed2f247c4fe77534aa7b407ad1bc
eliaszayoud-SE <eng.eliaszayoud@gmail.com>
2024-07-20 14:36:09 +0300

    add forget password end point

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('verify_email/', views.activation_user_email),
+    path('forget_password/', views.forget_password),
+    path('confirm_verify_code_reset_password/', views.confirm_verify_code_reset_password),
+    path('reset_password/', views.reset_password),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.core.mail import send_mail
+from django.conf import settings
 from rest_framework import status
+from .random import generate_random_verify_code
 from .models import *
 
 
@@ -9,9 +12,6 @@ def activation_user_email(request):
     user_email = request.data['email']
     
     verify_code = request.data['verify_code']
-    
-
-    
 
     user = CustomUser.objects.get(email=user_email)
 
@@ -26,37 +26,52 @@ def activation_user_email(request):
     print('Invalid')
     return Response({'message': 'Invalid verify_code.'}, status=status.HTTP_400_BAD_REQUEST)
 
-# Create your views here.
 
-# class UserProfileViewSet(ModelViewSet):
-#     queryset = UserProfile.objects.all()
-#     serializer_class = serializers.UserProfileSerializer
-#     permission_classes = [IsAuthenticated]
+@api_view(['POST'])
+def forget_password(request):
+    email = request.data['email']
+
+    user = CustomUser.objects.filter(email=email).first()
+    if user is not None :
+        user.verify_code = generate_random_verify_code()
+        user.save()
+        subject = 'welcome to our ecommerce'
+        message = f'Hi {user.username}, this is your new verify code  {user.verify_code} \n please use it to reset your  password'
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [user.email, ]
+        send_mail( subject, message, email_from, recipient_list )
+        return Response({'message': 'verify code send successfully.'},status=status.HTTP_200_OK)
     
-#     def get_queryset(self):
-#         return UserProfile.objects.filter(user_id=self.request.user.id)
+    return Response({'message': 'invalid email check if it.'},status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def confirm_verify_code_reset_password(request):
+    user_email = request.data['email']
     
-#     def get_serializer_context(self):
-#         return {
-#             'user_id':self.request.user.id
-#         }
+    verify_code = request.data['verify_code']
 
+    user = CustomUser.objects.filter(email=user_email, verify_code=verify_code).first()
+    if user is not None  :
+        return Response({'message': 'confirm verify code.'},status=status.HTTP_200_OK)
 
-    # @action(detail=False,methods=['GET', 'PUT', 'POST'], permission_classes=[IsAuthenticated])
-    # def me(self, request):
-    #     user_profile = UserProfile.objects.get(user_id=request.user.id)
-    #     if request.method == 'GET':
-    #         serializer = serializers.UserProfileSerializer(user_profile)
-    #         return Response(data=serializer.data)
-    #     elif request.method == 'PUT':
-    #         serializer = serializers.UserProfileSerializer(user_profile, data=request.data)
-    #         serializer.is_valid(raise_exception=True)
-    #         serializer.save()
-    #         return Response(data=serializer.data)
-    #     elif request.method == 'POST':
+    return Response({'message': 'Invalid verify_code or email .'}, status=status.HTTP_400_BAD_REQUEST)
 
-        
-            
+@api_view(['POST'])
+def reset_password(request):
+    user_email = request.data['email']
+    
+    verify_code = request.data['verify_code']
+
+    password = request.data['password']
+
+    user = CustomUser.objects.filter(email=user_email, verify_code=verify_code).first()
+    
+    if user is not None:
+        user.set_password(password)
+        user.save()
+        return Response({'message': 'Reset password is Done.'},status=status.HTTP_200_OK)
+    
+    return Response({'message': 'someting get wrong please try again letter.'},status=status.HTTP_400_BAD_REQUEST)
 
 
 
