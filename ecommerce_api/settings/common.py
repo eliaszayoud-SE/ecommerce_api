1803cfb53f4cbb3ba26129de1bdb937c67357182
eliaszayoud-SE <eng.eliaszayoud@gmail.com>
2024-07-21 15:20:21 +0300

    fix media url

--- ecommerce_api/settings/common.py
+++ ecommerce_api/settings/common.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MIDEA_URL = '/media/'
+MIDEA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
