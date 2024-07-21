2ce999329408c802d11a2e448a6b75473d7c7472
eliaszayoud-SE <eng.eliaszayoud@gmail.com>
2024-07-22 00:47:26 +0300

    modified prod.py

--- ecommerce_api/settings/prod.py
+++ ecommerce_api/settings/prod.py
@@ -14,3 +14,8 @@ DATABASES = {
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage'
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME' : os.environ['CLOUDINARY_CLOUD_NAME'],
+    'API_KEY' : os.environ['CLOUDINARY_API_KEY'],
+    'API_SECRET' : os.environ['CLOUDINARY_API_SECRET']
+}
