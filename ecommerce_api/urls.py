705defc3748d0b0541992591fb228100e40b27b4
eliaszayoud-SE <eng.eliaszayoud@gmail.com>
2024-07-21 16:32:58 +0300

    fix

--- ecommerce_api/urls.py
+++ ecommerce_api/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
