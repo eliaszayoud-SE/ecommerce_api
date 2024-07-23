594da0440d63a1e5d418be2a5167b05b691b38a4
eliaszayoud-SE <eng.eliaszayoud@gmail.com>
2024-07-23 15:34:59 +0300

    add item serializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Category
+from .models import Category, Item
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'name_ar', 'image', 'date']
+
+
+class ItemsSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    class Meta:
+        model = Item
+        fields = ['id', 'category', 'name', 'name_ar', 'description', 'description_ar', 'image', 'count', 'price', 'discount', 'date']
--- store/views.py
+++ store/views.py
@@ -3,17 +3,22 @@ from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, Item
+from .serializers import CategorySerializer, ItemsSerializer
 
 
 @api_view(['GET'])
 def home_data(request):
-    category = Category.objects.all()
-    cartegory_serializer = CategorySerializer(category, many=True)
+    categories = Category.objects.all()
+    cartegory_serializer = CategorySerializer(categories, many=True)
+
+    items = Item.objects.exclude(discount=0)
+    item_serializer = ItemsSerializer(items, many=True)
+
     
     data = {}
-    data['category'] = cartegory_serializer.data
+    data['categories'] = cartegory_serializer.data
+    data['items'] = item_serializer.data
     
     return Response(data)
 
