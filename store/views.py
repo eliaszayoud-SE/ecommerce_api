205573b118c7ea751c3a47c62defa4ca2fac2c20
eliaszayoud-SE <eng.eliaszayoud@gmail.com>
2024-07-24 16:49:52 +0300

    modified item view set

--- store/views.py
+++ store/views.py
@@ -31,3 +31,14 @@ class CategoryViewSet(ListModelMixin, GenericViewSet):
 class ItemsViewSet(ListModelMixin, GenericViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemsSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({'items':serializer.data})
