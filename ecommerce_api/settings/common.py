7d8bdd96aded55c09fca4c182f711c82246bc9a5
eliaszayoud-SE <eng.eliaszayoud@gmail.com>
2024-07-27 11:52:23 +0300

    modified access and refresh token time

--- ecommerce_api/settings/common.py
+++ ecommerce_api/settings/common.py
@@ -149,8 +149,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
-   'ACCESS_TOKEN_LIFETIME':timedelta(minutes=1),
-   'REFRESH_TOKEN_LIFETIME':timedelta(minutes=5)
+   'ACCESS_TOKEN_LIFETIME':timedelta(minutes=15),
+   'REFRESH_TOKEN_LIFETIME':timedelta(weeks=2)
 
 }
 
