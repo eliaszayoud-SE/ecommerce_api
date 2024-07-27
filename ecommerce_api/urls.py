6cbd6d0ab9114ea269637ebaf3bc9c5ad3719bb5
eliaszayoud-SE <eng.eliaszayoud@gmail.com>
2024-07-27 09:39:11 +0300

    fix error

--- ecommerce_api/urls.py
+++ ecommerce_api/urls.py
@@ -18,7 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from debug_toolbar.toolbar import debug_toolbar_urls
+# from debug_toolbar.toolbar import debug_toolbar_urls
 
 
 urlpatterns = [
